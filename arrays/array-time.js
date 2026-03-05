0586636612f93fa5cea954bd0ac4947dd9a66ce5
nycollasalboit11-ops <nycollasalboit11@gmail.com>
2026-03-05 14:15:57 +0000

    finalizar if-else e introdução aos arrays

---
+++ arrays/array-time.js
@@ -0,0 +1,10 @@
+// um array é um grupo indexado de valores
+// podem ser homogêneo / heteogêneo
+//seus índices  vão de "0" ... "infinito"
+//possuem funçõe nativas (prontas)
+//podem ser iterados e checados
+// inices     0       1       2
+const time = ['maria' ,'clara', 'maria' true, 56]
+console.log (time)
+console.log (time[1])
+console.log(time [32])
---
+++ condicionais/if-login.js
@@ -0,0 +1,10 @@
+// veriica se LOGIN e SENHA corresponem ao mesmo tempo (&&)
+
+const login = 'superman'
+const senha = 'segredo123'
+
+if (login == 'superman' && senha == 'segredo123') {
+    console.log ("acesso permitido!")
+} else {
+ console.warn ('verifique seus dados')
+}
